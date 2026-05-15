e45a3628424848ce878a1998254293304608e434
DT1991 <42806980+DT1991@users.noreply.github.com>
2026-05-15 23:40:08 +0800

    add required_version to all modules

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -3,6 +3,7 @@
 ###############################################################################
 
 terraform {
+  required_version = ">= 1.10.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
